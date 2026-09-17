f7d9123f9d929fb200891ba313a8136774e598dd
Tharanga Jayawardhana <chathuratharanga076@gmail.com>
2026-09-17 12:19:42 +0530

    setuped the mongo db connection and update the gitignore file to prevent the staging the cache,log etc filess

---
+++ api/SmartSolarMicrogrid.API/Data/MongoDbSettings.cs
@@ -0,0 +1,7 @@
+namespace SmartSolarMicrogrid.API.Data;
+
+public class MongoDbSettings
+{
+    public string ConnectionString { get; set; } = null!;
+    public string DatabaseName { get; set; } = null!;
+}
